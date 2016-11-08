95bba32541a9f26a560b96d89717ce70c7cf92f2
k-okina <okinakahiro@gmail.com>
2016-11-08 13:14:38 +0900

    created wrap method

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * ラッピングしてないやつ
  * @param  obj or array
@@ -11,4 +12,19 @@ function notWrap($data_) : array
         'code'    => 200,
         'message' => 'success get datas'
     ]);
+}
+
+/**
+ * ラッピングした奴
+ * @param  mixed
+ * @return array
+ */
+function wrap($data_) : array
+{
+    return [
+        'status'  => 'success',
+        'code'    => 200,
+        'data'    => $data_,
+        'message' => 'success get datas'
+    ];
 }
